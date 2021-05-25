9f4b4eb6ad3f1769e36fc6491646115b49bc184e
rifkyfu <rifky2fuady@gmail.com>
2021-05-25 08:30:30 +0700

    clean up eslint

--- src/scripts/routes/url-parser.js
+++ src/scripts/routes/url-parser.js
@@ -1,16 +1,16 @@
 const UrlParser = {
   parseActiveUrlWithCombiner() {
     const url = window.location.hash.slice(1).toLowerCase();
-    const splitedUrl = this._urlSplitter(url);
-    return this._urlCombiner(splitedUrl);
+    const splitedUrl = this.urlSplitter(url);
+    return this.urlCombiner(splitedUrl);
   },
 
   parseActiveUrlWithoutCombiner() {
     const url = window.location.hash.slice(1).toLowerCase();
-    return this._urlSplitter(url);
+    return this.urlSplitter(url);
   },
 
-  _urlSplitter(url) {
+  urlSplitter(url) {
     const urlsSplits = url.split('/');
     return {
       resource: urlsSplits[1] || null,
@@ -19,7 +19,7 @@ const UrlParser = {
     };
   },
 
-  _urlCombiner(splitedUrl) {
+  urlCombiner(splitedUrl) {
     return (splitedUrl.resource ? `/${splitedUrl.resource}` : '/')
     + (splitedUrl.id ? '/:id' : '')
     + (splitedUrl.verb ? `/${splitedUrl.verb}` : '');
--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -4,24 +4,24 @@ import routes from '../routes/routes';
 
 class App {
   constructor({ button, drawer, content }) {
-    this._button = button;
-    this._drawer = drawer;
-    this._content = content;
-    this._initialAppShell();
+    this.button = button;
+    this.drawer = drawer;
+    this.content = content;
+    this.initialAppShell();
   }
 
-  _initialAppShell() {
+  initialAppShell() {
     DrawerInitiator.init({
-      button: this._button,
-      drawer: this._drawer,
-      content: this._content,
+      button: this.button,
+      drawer: this.drawer,
+      content: this.content,
     });
   }
 
   async renderPage() {
     const url = UrlParser.parseActiveUrlWithCombiner();
     const page = routes[url];
-    this._content.innerHTML = await page.render();
+    this.content.innerHTML = await page.render();
     await page.afterRender();
   }
 }
